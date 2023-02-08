d76e3bdb90c30932c5ec4ddbf1bec5ccfd272440
shatrunjaysrivastava <shatrunjay.srivastava@cogoport.com>
2023-02-08 15:03:49 +0530

    aoi-react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import axios from "axios";
+import React, {useState, useEffect} from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    const [movies, setMovies] = useState([]);
+    const [text_s, setText_s] = useState("call");
+    const [likedMovies, setLikedMovies] = useState([]);
+    
+
+    const getMovies = async () => {
+        {text_s === "" ? setText_s() : text_s = text_s}
+        const surl = `https://www.omdbapi.com/?s=${text_s}&apikey=48710dad`;
+        const moviesRequest = await axios.get(
+            surl
+        );
+        console.log(moviesRequest.data.Search);
+        setMovies(moviesRequest.data.Search);
+        // console.log({movies});
+    }
+
+    useEffect(() => {
+        getMovies();
+    }, []);
+
+    return (
+        <div>
+            <input type = "text"
+                value = {text_s}
+                onChange = {(e) => {setText_s(e.target.value)}}
+                placeholder = "Search Movies"
+            />
+            <button onClick = { getMovies }> Search </button>
+
+            {movies.length > 0 ? movies.map(movie => {
+                return (
+                    <div>
+                        <div>
+                            <div>
+                                <img alt = "Search" src = { movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400' } />
+                            </div>
+                            <div>
+                                <span>{ movie.Type }</span>
+                                <h3>{ movie.Title }</h3>
+                                <button onClick = { setLikedMovies() }> Search </button>
+                            </div>
+                        </div>
+                    </div>
+
+                )
+            }): ''}
+        </div> 
+    )
+}
+ReactDOM.render(<App />, document.getElementById('root'));
